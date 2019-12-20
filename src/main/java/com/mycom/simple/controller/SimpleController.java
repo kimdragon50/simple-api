7848315d05f6948380d9633ac0299e6d372799a9
남궁성환 <saturn203@naver.com>
2019-12-20 15:50:12 +0900

    v3.1.41

--- src/main/java/com/mycom/simple/controller/SimpleController.java
+++ src/main/java/com/mycom/simple/controller/SimpleController.java
@@ -41,7 +41,7 @@ public class SimpleController {
 	@GetMapping("/version")
 	public String version(){
 		//log.info("version 6.0");
-		return "=====  version 3.1.9 !-----";
+		return "=====  version v3.1.41 !-----";
 		
 	}
 
